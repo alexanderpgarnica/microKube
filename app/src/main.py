f77ff063460826fe068e07a3ea926392f5ea1325
Peter Palacios <peterg028@hotmail.com>
2026-03-05 20:28:16 -0500

    cicd test changed message

--- app/src/main.py
+++ app/src/main.py
@@ -8,5 +8,5 @@ def root():
     return {
         "service": os.getenv("SERVICE_NAME", "microservice"),
         "env": os.getenv("ENV", "dev"),
-        "message": "OK Todo funcionando de forma correcta."
+        "message": "OK desplegado por CI/CD con ArgoCD 😎"
     }
